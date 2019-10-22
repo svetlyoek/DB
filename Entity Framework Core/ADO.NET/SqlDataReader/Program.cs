9920433d6514449538de15d1f4584e7efea6a8f7
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-10-22 13:11:51 +0300

    add new

---
+++ Entity Framework Core/ADO.NET/SqlDataReader/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SqlDataReaderDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Server=.\\MSSQLSERVER01;" + "Database=SoftUni;" + "Integrated Security=true;");
+
+            connection.Open();
+
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand("select * from Employees", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        string firstName = (string)reader["FirstName"];
+                        string lastName = (string)reader["LastName"];
+                        decimal salary = (decimal)reader["Salary"];
+                        Console.WriteLine($"{firstName} {lastName} - {salary:f2}");
+                    }
+                }
+
+            }
+        }
+    }
+}
