a9813d90b6ac39ff539747a08a6225a9471b5d9c
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-11-05 19:13:29 +0200

    Add new

---
+++ Entity Framework Core/Code-first/HospitalDatabase/HospitalDatabase/Migrations/20191103164447_DoctorPasswordAdded.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalDatabase.Migrations
+{
+    public partial class DoctorPasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Doctor",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Doctor");
+        }
+    }
+}
