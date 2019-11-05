ff9437f0386ee268449f7a60e14a88d3a7c94cfe
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-11-05 11:51:42 +0200

    remove item

--- Entity Framework Core/Code-first/HospitalDatabase/HospitalDatabase/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P01_HospitalDatabase.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString = "Server=DESKTOP-JH4M4M9\\MSSQLSERVER01;Database=HospitalDatabase;Integrated Security=true;";
-    }
-}
