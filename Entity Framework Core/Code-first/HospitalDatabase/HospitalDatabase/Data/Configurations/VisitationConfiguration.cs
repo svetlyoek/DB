bd8f62d6ff965b01c56003996b154863eab7622a
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-11-05 13:15:17 +0200

    add new

---
+++ Entity Framework Core/Code-first/HospitalDatabase/HospitalDatabase/Data/Configurations/VisitationConfiguration.cs
@@ -0,0 +1,34 @@
+﻿namespace HospitalDatabase.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using P01_HospitalDatabase.Data.Models;
+
+    public class VisitationConfiguration : IEntityTypeConfiguration<Visitation>
+    {
+        public void Configure(EntityTypeBuilder<Visitation> builder)
+        {
+            builder
+               .HasKey(k => k.VisitationId);
+
+            builder
+                 .Property(p => p.Date)
+                .IsRequired(true);
+
+            builder
+                 .Property(p => p.Comments)
+                .HasMaxLength(250)
+                .IsUnicode();
+
+            builder
+                .HasOne(v => v.Patient)
+                .WithMany(v => v.Visitations)
+                .HasForeignKey(fk => fk.PatientId);
+
+            builder
+                 .HasOne(d => d.Doctor)
+                .WithMany(v => v.Visitations)
+                .HasForeignKey(fk => fk.DoctorId);
+        }
+    }
+}
