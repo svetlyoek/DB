422c3b4a188621e1e8fdb28b2c4ff46ee9a427af
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-11-05 11:52:07 +0200

    Add item

---
+++ Entity Framework Core/Code-first/HospitalDatabase/HospitalDatabase/Data/Models/Doctor.cs
@@ -0,0 +1,19 @@
+﻿namespace P01_HospitalDatabase.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Specialty { get; set; }
+
+        [MinLength(4)]
+        public string Password { get; set; }
+
+        public ICollection<Visitation> Visitations { get; set; } = new HashSet<Visitation>();
+    }
+}
