b5626d9316a2391daec7c9cc5438a7691ea1a075
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-11-06 16:45:24 +0200

    Remove item

--- Entity Framework Core/Entity relations/StudentSystem/P01_StudentSystem.Data/Configurations/ConnectionString.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P01_StudentSystem.Data.Configurations
-{
-
-    internal static class ConnectionString
-    {
-        public static string connectionString = "Server=DESKTOP-JH4M4M9\\MSSQLSERVER01;Database=StudentSystem;Integrated Security=True;";
-    }
-}
