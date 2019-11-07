5e9269ecb765f7223fc52bae7c652da6005683a5
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-11-07 17:56:57 +0200

    Remove item

--- Entity Framework Core/Entity relations/FootballBetting/P03_FootballBetting.Data/Configurations/ConnectionConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P03_FootballBetting.Data.Configurations
-{
-    public static class ConnectionConfiguration
-    {
-        public static string connectionString = "Server=DESKTOP-JH4M4M9\\MSSQLSERVER01;Database=FootballBetting;Integrated Security = True;";
-    }
-}
