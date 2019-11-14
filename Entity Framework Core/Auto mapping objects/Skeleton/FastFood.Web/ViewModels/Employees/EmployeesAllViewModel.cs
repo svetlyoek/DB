e03b134a846c1208fcbabe3eb4016e5ed6ac2e7a
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-11-14 19:52:29 +0200

    Add new

---
+++ Entity Framework Core/Auto mapping objects/Skeleton/FastFood.Web/ViewModels/Employees/EmployeesAllViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Web.ViewModels.Employees
+{
+    public class EmployeesAllViewModel
+    {
+        
+        public string Name { get; set; }
+
+        
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+
+     
+        public string Position { get; set; }
+    }
+}
