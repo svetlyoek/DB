8870535c814feb90d22a84e3ce9e75eae86fb460
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-12-01 18:10:18 +0200

    Remove item

--- Entity Framework Core/Best practicing and architecture/PetStore/PetStore.Data/Configurations/Connection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PetStore.Data.Configurations
-{
-    public static class Connection
-    {
-        public const string ConnectionString = "Server=DESKTOP-JH4M4M9\\MSSQLSERVER01;Database=PetStore;Integrated Security=True;";
-    }
-}
